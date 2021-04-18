27e415a20eebf8075b7babd29da00e62b72fb6ac
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-18 13:37:30 -0700

    dynamic toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,16 +4,31 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false)
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if (showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`
+        }
+        else {
+            linksContainerRef.current.style.height = "0px"
+        }
+    }, [showLinks])
+
     return <nav>
         <div className="nav-center">
             <div className="nav-header">
                 <img src={logo} alt="logo" />
-                <button className="nav-toggle">
+                <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
                     <FaBars />
                 </button>
             </div>
-            <div className="links-conter show-container">
-                <ul className="links">
+            <div className="links-container" ref={linksContainerRef}>
+                {/* <div className={`${showLinks ? "links-container show-container" : "links-container "}`}> */}
+                <ul className="links" ref={linksRef}>
                     {links.map((link) => {
                         const { id, url, text } = link;
                         return <li key={id}>
@@ -22,6 +37,7 @@ const Navbar = () => {
                     })}
                 </ul>
             </div>
+
             <ul className="social-icons">
                 {social.map((socialIcon) => {
                     const { id, url, icon } = socialIcon;
